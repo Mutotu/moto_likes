83221bcc7de031c53003af630ae6e66a552c772c
mutotu <muttalip.aydemir@gmail.com>
2022-03-11 09:37:37 +0000

    inital set up

---
+++ moto_likes/motobook/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MotobookConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'motobook'
---
+++ moto_likes/motobook/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def likes(request):
+    return HttpResponse("Testing for motorclye website")
